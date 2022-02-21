7cc4b6cfb224cd75807c350421afd36dfbfee796
Généreux Jean-Paul <jpg240sx@gmail.com>
2022-02-21 17:10:09 -0500

    Création classe point avec attributs

---
+++ Exercice4.js
@@ -0,0 +1,19 @@
+/**
+ * Description: Exercice 4 Distance. Créer une classe point. Une seule méthode "distance(point)."
+ * Auteur: Jean-Paul Généreux
+ * Date 21 février 2022
+ */
+
+class point
+{
+    constructor(coordX, coordY)
+    {
+        this.coordX = coordX;
+        this.coordY = coordY;
+    }
+}
+
+var point1 = new point(2, 2);
+var point2 = new point(5, 6)
+
+console.log(point1);
